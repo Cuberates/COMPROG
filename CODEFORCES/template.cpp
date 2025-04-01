78125651a00eba8fd7ffe63f9e3d428a47ed861d
Duc Xuan Viet Nguyen <cookie@d207-023-163-179.wireless.sfu.ca>
2025-04-01 13:42:51 -0700

    fixed the template code

---
+++ CODEFORCES/template.cpp
@@ -0,0 +1,35 @@
+#include <iostream> 
+#include <vector> 
+#include <map>
+#include <cmath> 
+#include <stack>
+#include <queue>
+#include <string> 
+#include <random> 
+
+#define ll long long
+#define vect vector
+#define MOD 1e9+7
+
+void solve(); 
+
+int main(void) { 	
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0); std::cin.tie(0);
+
+	int tests = 1;
+	std::cin >> tests; 
+	for(int i = 0; i < tests; i++) {
+		#ifndef ONLINE_JUDGE
+		for(int j = 0; j < 10; j++)
+			std::cout << "-";
+		#endif
+		std::cout << "\n";
+		solve(); 
+	}	
+}
+
+void solve() { // SOLUTION GOES HERE
+	
+}
+
