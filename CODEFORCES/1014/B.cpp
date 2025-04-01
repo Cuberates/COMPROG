ab1a4db7d77631f7e5d14cf7c9273bab45632ddc
Duc Xuan Viet Nguyen <cookie@d207-023-163-179.wireless.sfu.ca>
2025-04-01 13:39:02 -0700

    added B for CF1014

---
+++ CODEFORCES/1014/B.cpp
@@ -0,0 +1,52 @@
+#include <iostream> 
+#include <vector> 
+#include <map>
+#include <cmath> 
+#include <stack>
+#include <queue>
+#include <string> 
+#include <random> 
+
+#define ll long long
+#define vect vector
+#define MOD 1e9+7
+
+void solve(); 
+
+int main(void) { 	
+	std::ios_base::sync_with_stdio(false);
+	std::cout.tie(0); std::cin.tie(0);
+
+	int tests = 1;
+	std::cin >> tests; 
+	for(int i = 0; i < tests; i++) {
+		#ifndef ONLINE_JUDGE
+		for(int j = 0; j < 10; j++)
+			std::cout << "-";
+		#endif
+		std::cout << "\n";
+		solve(); 
+	}	
+}
+
+void solve() { 
+	int n;
+	std::string a, b; 
+	int nao = 0, nbo = 0, nae = 0, nbe = 0;
+	std::cin >> n >> a >> b; 
+	for(int i = 0; i < n; i++) { 
+		if(a[i] == '1') { 
+			if(i % 2 == 0) nae++; 
+			else nao++; 
+		}
+	}
+	for(int i = 0; i < n; i++) { 
+		if(b[i] == '0') { 
+			if(i % 2 == 0) nbe++; 
+			else nbo++; 
+		}
+	}
+	if(nao <= nbe && nae <= nbo) std::cout << "YES \n";
+	else std::cout << "NO \n";
+}
+
