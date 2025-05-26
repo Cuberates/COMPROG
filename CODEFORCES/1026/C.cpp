7647ffab04490d313d5e9e185fa1048811fa4266
Duc Xuan Viet Nguyen <cookie@cuberates-5.local>
2025-05-26 12:18:24 +0700

    solving 1026C

--- CODEFORCES/1026/C.cpp
+++ CODEFORCES/1026/C.cpp
@@ -10,63 +10,45 @@ using namespace std;
 #define pii pair<int,int> 
 
 vector<pii> o; 
-vector<int> p, t; 
+vector<int> d; 
 int n; 
 
-int check() {
-   int h = 0; 
-   for(int i = 0; i < n; i++) {
-      // if(i > 0) h += (t[i] - t[i-1]);
-      h += (t[i]);
-      // cout << h << " " << o[i].first << " " << o[i].second << "\n";
-      if (h >= o[i].first && h <= o[i].second) continue; 
-      else if (h < o[i].first) return 0; 
-      else return -1; 
-   }
-   return 1; 
-}
-void construct(int v) {
-   t = p; 
-   for(int i = 0; i < t.size(); i++) {
-      if (t[i] == -1 && i <= v) t[i] = 1; 
-      else if (t[i] == -1 && i > v) t[i] = 0;
-   }
-}
 void gabagoo() {
-   cin >> n;  
-   p = vector<int>(n);
-   for(int i = 0; i < n; i++) 
-      cin >> p[i];
+   cin >> n; 
+   d = vector<int>(n);
+   for(int i = 0; i < n; i++)
+      cin >> d[i];
    o = vector<pii>(n);
+   vector<int> lb(n), ub(n);
    for(int i = 0; i < n; i++) { 
       int l, r; 
       cin >> l >> r; 
-      o[i] = {l, r};  
+      lb[i] = l; 
+      ub[i] = r; 
    }
-   int lb = -1, ub = n+1; 
-   int good = 0;
-   while (lb <= ub) {
-      int mid = (lb + ub) / 2; 
-      construct(mid); 
-      int c = check(); 
-      if (c > 0) { 
-         good = 1; 
-         break; 
-      }
-      else if (c == 0) { lb = mid + 1; }
-      else {ub = mid - 1; }
-
-      for(int i = 0; i < n; i++) { 
-         cout << t[i] << " "; 
+   int L = 0, R = 0; 
+   stack<int> st; 
+   int ok = true; 
+   for(int i = 0; i < n && ok; i++) {
+      if (L < lb[i]) {
+         L = lb[i];
+         st.push(1);
+      } else st.push(0);
+      if (R > ub[i]) {
+         R = ub[i];
+         st.push(0);
+      } else st.push(0); 
+      if (L > R) ok = false; 
+   }
+   if(!ok) cout << "-1\n";
+   else { 
+      while (!st.empty()) {
+         cout << st.top() << " ";
+         st.pop(); 
       }
       cout << "\n";
    }
-   if (good) {
-      for(int i = 0; i < n; i++)
-         cout << t[i] << " "; 
-      cout << "\n";
-   } else cout << "-1 \n";
-}    
+}     
 
 int main(void) { 	
    ios_base::sync_with_stdio(false);
